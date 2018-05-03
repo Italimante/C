bf3486479bf0baf44145b7757cd6602d3c59ef71
alumno <alumno>
2018-05-03 12:11:30 -0300

    hacer funciones genericas en dos archivos .h y .c

---
+++ myGenericShit/eShity.h
@@ -0,0 +1,15 @@
+#ifndef ESHITY_H_INCLUDED
+#define ESHITY_H_INCLUDED
+struct typedef{
+    char nombre[50];
+    int shityId;
+}eShity;
+
+
+
+#endif // ESHITY_H_INCLUDED
+
+/* FUNCIONES A HACER:
+
+
+*/
---
+++ myGenericShit/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "eShity.h"
+#define SIZE 30
+
+int main()
+{
+    printf("Hello world!\n");
+    return 0;
+}
